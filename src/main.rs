0e84559fbd8854e666a09716eef8ea0b48fe39ae
drademacher <danny.rademacher@senacor.com>
2020-12-07 17:24:29 +0100

    Solve day 5

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_05;
 
 use std::env;
 
@@ -12,6 +13,7 @@ fn main() {
         1 => println!("{}", day_01::solve()),
         2 => println!("{}", day_02::solve()),
         3 => println!("{}", day_03::solve()),
+        5 => println!("{}", day_05::solve()),
         _ => println!("Invalid input"),
     };
 }
