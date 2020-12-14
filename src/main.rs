67a34016a1a4ee84cacf767617ccdb2c6435c6b6
drademacher <danny.rademacher@senacor.com>
2020-12-14 14:12:27 +0100

    Solve day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod day_01;
 mod day_02;
 mod day_03;
+mod day_04;
 mod day_05;
 mod day_06;
 mod day_07;
@@ -15,6 +16,7 @@ fn main() {
         1 => println!("{}", day_01::solve()),
         2 => println!("{}", day_02::solve()),
         3 => println!("{}", day_03::solve()),
+        4 => println!("{}", day_04::solve()),
         5 => println!("{}", day_05::solve()),
         6 => println!("{}", day_06::solve()),
         7 => println!("{}", day_07::solve()),
